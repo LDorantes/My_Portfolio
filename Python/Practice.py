078e8e4ced036261ca20b7e2a9103dfd897d765a
Leo Dorantes <leodoor2@gmail.com>
2024-12-03 12:17:43 -0600

    modified

--- Python/Practice.py
+++ Python/Practice.py
@@ -8,15 +8,14 @@ import random
 import re
 import sys
 
+n = int(input().strip())
 
-
-if __name__ == '__main__':
-    n = int(input().strip())
-    if n % 2 != 0:
-        print("Weird")
-    elif n % 2 == 0 and 2 <= n <= 5:
-        print("Not Weird")
-    elif n % 2 == 0 and 6 <= n <= 20:
-        print("Weird")
-    elif n % 2 == 0 and n > 20:
-        print("Not Weird")
+# Las condiciones van aquí
+if n % 2 != 0:
+    print("Weird")
+elif n % 2 == 0 and 2 <= n <= 5:
+    print("Not Weird")
+elif n % 2 == 0 and 6 <= n <= 20:
+    print("Weird")
+elif n % 2 == 0 and n > 20:
+    print("Not Weird")
